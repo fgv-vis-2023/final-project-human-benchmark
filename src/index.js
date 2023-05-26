46b89b1202672e7bc527b9196ac9fa9bd6e67d39
barrafas <barradastiago15@gmail.com>
2023-05-26 00:25:37 -0300

    installing firebase (but not doing anything with it yet)

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// run 'npm run build' to bundle this file
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJnXNrYtlXC-DybP5MAJZXuYKHqcczpoE",
+    authDomain: "mind-track-778eb.firebaseapp.com",
+    projectId: "mind-track-778eb",
+    storageBucket: "mind-track-778eb.appspot.com",
+    messagingSenderId: "960246747563",
+    appId: "1:960246747563:web:f659460f312b7fd89b42ee"
+  };
+
+  initializeApp(firebaseConfig);
