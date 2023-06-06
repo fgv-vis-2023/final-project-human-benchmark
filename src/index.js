ee6f19059d12f159990ceb69b677cb09efadffcf
barrafas <barradastiago15@gmail.com>
2023-06-05 23:21:03 -0300

    clean up de coisas inuteis da pagina

--- src/index.js
+++ src/index.js
@@ -30,97 +30,6 @@ initializeApp(firebaseConfig);
 const db = getFirestore();  // connect to firestore
 const auth = getAuth();  // connect to auth
 
-
-const colRef = collection(db, 'tests');  // connect to collection 'users'
-
-// const q = query(colRef, where("Game 3 Score", ">", 20), orderBy('Game 3 Score', 'asc'))  // make queries
-
-const q = query(colRef, orderBy('createdAt', 'asc'))
-
-// gets snapshot on page load
-// getDocs(colRef)
-//   .then((snapshot) => {
-//     let userscores = []
-//     snapshot.docs.forEach((doc) => {
-//       userscores.push({ id: doc.id, ...doc.data()})  // push the data of each document to the array, the three dots are the spread operator
-//     })
-//     console.log(userscores)
-//   })
-//   .catch((error) => {
-//     console.log(error.message)
-//   })
-
-
-// gets snapshot of a query every time the database changes
-onSnapshot(q, (snapshot) => {let userscores = []
-  snapshot.docs.forEach((doc) => {
-    userscores.push({ id: doc.id, ...doc.data()})  // push the data of each document to the array, the three dots are the spread operator
-  })
-  console.log(userscores)
-})
-
-// add a document
-const sendData = document.querySelector('.add');
-sendData.addEventListener('submit', (e) => {
-  e.preventDefault()  // stops page from reloading
-
-  addDoc(colRef, {
-    Username: auth.currentUser.email,
-    "Game 1 Score": sendData["Game 1 Score"].value,
-    "Game 2 Score": sendData["Game 2 Score"].value,
-    "Game 3 Score": sendData["Game 3 Score"].value,
-    "Game 4 Score": sendData["Game 4 Score"].value,
-    "Game 5 Score": sendData["Game 5 Score"].value,
-    createdAt: serverTimestamp(),
-  })
-  .then(() => {
-    sendData.reset()
-  })
-})
-
-// delete a specific document
-const delData = document.querySelector('.delete');
-delData.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  const docRef = doc(db, 'tests', delData.id.value)
-
-  deleteDoc(docRef)
-  .then(() => {
-    delData.reset()
-  })
-})
-
-// get a single specific document
-const docRef = doc(db, 'tests', 'mgrHldHkenr52jbhK1po');
-
-onSnapshot(docRef, (doc) => {
-  console.log(doc.data(), doc.id)
-})
-
-// update a specific document
-const updateData = document.querySelector('.update');
-
-updateData.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  const docRef = doc(db, 'tests', updateData.id.value)
-  // uses docRef to get the document, checks its fields, then checks if the field to be updated is already present
-  getDoc(docRef)
-    .then((doc) => {
-    const keys = Object.keys(doc.data())
-    if (keys.includes(updateData.field.value)) {
-      updateDoc(docRef, {
-        [updateData.field.value]: updateData.value.value
-      })
-    }
-    else {
-      console.log('invalid field')
-    }
-  })
-})
-
-// authentication
 const signupForm = document.querySelector('.signup');
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -187,39 +96,8 @@ loginForm.addEventListener('submit', (e) => {
 //   }
 // })
 
-// logic to get scores of a specific user in a game
-const userquery = document.querySelector('.consult');
-userquery.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const scores = collection(db, 'mock');  // connect to specific game collection 
-  
-  const qScores = query(scores, 
-                        where("usuario", "==", userquery.email.value))  // make queries
-  
-  onSnapshot(qScores, (snapshot) => {
-    let userscores = []
-    snapshot.docs.forEach((doc) => {
-      userscores.push({"score": doc.data()[`jogo${userquery.gameid.value}`],
-                        // "jogo2": doc.data().jogo2,
-                        // "jogo3": doc.data().jogo3,
-                        // "jogo4": doc.data().jogo4,
-                        // "jogo5": doc.data().jogo5,
-                        "emocao": doc.data().emocao,
-                        "exercicio": doc.data().exercicio,
-                        "sono": doc.data().sono, 
-                        "dia": doc.data().dia})
-                      })
-                      userscores = userscores.sort(function(a, b){
-                        if (a.dia > b.dia) {return 1}
-                        return -1
-                      })
-                      console.log(userscores)
-                    })
-                  })
                   
 // Radial graph
-
-
 var radarmargin = {top: 100, right: 100, bottom: 100, left: 100},
 radarwidth = Math.min(700, window.innerWidth - 10) - radarmargin.left - radarmargin.right,
 radarheight = Math.min(radarwidth, window.innerHeight - radarmargin.top - radarmargin.bottom - 20);
